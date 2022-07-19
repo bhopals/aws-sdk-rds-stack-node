1841df07f0232122619e370ab7aeab5d4c35b1ce
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-07-19 10:43:48 -0400

    connect lambda to rds

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -9,6 +9,8 @@ import {
   UTF_8,
 } from "../../lib/stackConfiguration";
 
+var mysql = require("mysql2/promise");
+
 export async function main(
   event: APIGatewayProxyEventV2,
   context: any
@@ -25,31 +27,67 @@ export async function main(
   console.log("userName 👉", process.env.userName);
   console.log("password 👉", process.env.password);
 
+  const sqlConfig = {
+    user: process.env.userName,
+    password: process.env.password,
+    database: process.env.databaseName,
+    host: process.env.endpoint,
+    // pool: {
+    //   max: 10,
+    //   min: 0,
+    //   idleTimeoutMillis: 30000,
+    // },
+    // options: {
+    //   encrypt: true, // for azure
+    //   trustServerCertificate: false, // change to true for local dev / self-signed certs
+    // },
+  };
+
   let response;
   try {
-    const lambdaClient = new Lambda({});
-    const { Payload } = await lambdaClient.invoke({
-      FunctionName: `${appName}-${LambdaType.PRIVATE_LAMBDA}`,
-      InvocationType: INVOCATION_TYPE,
-      Payload: new TextEncoder().encode(JSON.stringify({ event, context })),
-    });
+    // make sure that any items are correctly URL encoded in the connection string
+    console.log("BEFORE>sqlConfig>", sqlConfig);
+    let connection = await mysql.createConnection(sqlConfig);
+    console.log("connect>>", connection);
+
+    // //CREATE DATABASE
+    // const db = await connection.query(
+    //   `CREATE DATABASE mydb${Math.floor(Math.random() * 100)}`
+    // );
+    // const tb = await connection.query(
+    //   `CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))`
+    // );
+
+    // const rec = await connection.query(
+    //   `INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')`
+    // );
+
+    // const result = await connection.query(`select * from customers`);
+    const [result, fields] = await connection.execute(
+      "SELECT * FROM `table` WHERE `name` = ? AND `age` > ?",
+      ["Morty", 14]
+    );
+
+    console.dir("result>", result);
+    console.dir("fields>", fields);
 
     response = {
       body: JSON.stringify({
-        data: JSON.parse(new TextDecoder(UTF_8).decode(Payload) || "{}"),
+        result,
+        fields,
       }),
       statusCode: 200,
     };
-  } catch (error) {
+  } catch (err) {
+    console.log("ERR>", err);
     response = {
       body: JSON.stringify({
-        error,
+        err,
       }),
       statusCode: 500,
     };
   }
 
   console.log(response);
-
   return response;
 }
