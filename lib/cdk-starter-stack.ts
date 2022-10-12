0dc848c4c7689a73616a112881ffece9fdbdbca8
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-10-12 08:58:52 -0400

    add S3 Bucket

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -116,7 +116,7 @@ export class CdkStarterStack extends cdk.Stack {
       value: bucket.bucketName,
     });
 
-    /** OUTPUT S3 BUCKET NAME Public BUCKET name region az*/
+    /** OUTPUT S3 BUCKET NAME Public BUCKET name region with multiple az */
     new CfnOutput(this, S3_BUCKET_ARN, {
       value: bucket.bucketArn,
     });
